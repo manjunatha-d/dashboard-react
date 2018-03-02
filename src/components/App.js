90780c0af4cc387c53e456620fcf800ccce58f31
Manjunatha D <manjunathad13@gmail.com>
2018-03-02 10:46:02 +0530

    Make it responsive

--- src/components/App.js
+++ src/components/App.js
@@ -7,59 +7,59 @@ class App extends React.Component {
       <div className="dashboard">
         <h2 className="dashboard__page-title">Dashboard</h2>
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Blog Post" subtitle="400-500 Words" icon="fa-clipboard" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="White Paper" subtitle="2500-3000 Words" icon="fa-sticky-note" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Website Content" subtitle="400-500 Words" icon="fa-laptop" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Press Release" subtitle="400-500 Words" icon="fa-newspaper" />
           </div>
         </div>
 
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Product Description" subtitle="400-500 Words" icon="fa-align-center" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Social Media" subtitle="400-500 Words" icon="fa-tablet-alt" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Book/Story Writing" subtitle="400-500 Words" icon="fa-book" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Video Script" subtitle="400-500 Words" icon="fa-video" />
           </div>
         </div>
 
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Newsletter" subtitle="400-500 Words" icon="fa-newspaper" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Brochure Content" subtitle="400-500 Words" icon="fa-address-book" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Custom Content" subtitle="400-500 Words" icon="fa-wrench" />
           </div>
-          <div className="col-md-3">
+          <div className="col-lg-3 col-sm-6 col-xs-12">
             <Item title="Contest" subtitle="400-500 Words" icon="fa-trophy" />
           </div>
         </div>
 
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-lg-6 col-xs-12">
             <Item
               title="Order from Template"
               subtitle="You can give an order from templates"
               icon="fa-bookmark"
             />
           </div>
-          <div className="col-md-6">
+          <div className="col-lg-6 col-xs-12">
             <Item
               title="Orde via Spreadsheets"
               subtitle="It's useful when giving Bulk Orders"
